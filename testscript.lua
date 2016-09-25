929f12afef85f44bcced1d5ad70e1705a2a8390a
Xingyuan Bu <buyuantb@163.com>
2016-09-25 16:15:01 +0800

    check LLCNet gradient updata

--- testscript.lua
+++ testscript.lua
@@ -5,22 +5,46 @@ allc = model:get(6)
 bencode = bllc.output
 aencode = llc.output
 
---print "================== before encode =================="
---print(bencode)
-print "================= after encode =================="
-print(aencode)
+isprintbeforeencode = false
+isprintafterencode = false
+isprintgradinput = true
+print "################# LLCNet ##################"
+print "################# LLCNet ##################"
+print "################# LLCNet ##################"
+if isprintbeforeencode then
+   print "================== before encode =================="
+   print(bencode)
+end
+if isprintafterencode then 
+   print "================= after encode =================="
+   print(aencode)
+end
+if isprintgradinput then
+   print "================= after gradinput =================="
+   print(llc.gradInput)
+end
 singlellc = nn.LLC(llc.B)
 output = torch.zeros(aencode:size(1),bencode:size(2),bencode:size(3))
+gradInput = torch.zeros(bencode:size(1),bencode:size(2),bencode:size(3))
 
-print "================== single LLC =================="
-print "================== single LLC =================="
-print "================== single LLC =================="
+print "################# single LLC ##################"
+print "################# single LLC ##################"
+print "################# single LLC ##################"
 for i=1,bencode:size(2) do
    for j=1,bencode:size(3) do
-      --print "================== before encode =================="
-      --print(bencode[{{},i,j}])
+      if isprintbeforeencode then
+         print "================== before encode =================="
+         print(bencode[{{},i,j}])
+      end
       output[{{},i,j}] = singlellc:forward(bencode[{{},i,j}])
-      print "================= after encode =================="
-      print(output[{{},i,j}])
+      if isprintafterencode then
+         print "================= after encode =================="
+         print(output[{{},i,j}])
+      end
+      if isprintgradinput then
+         gradInput[{{},i,j}] = singlellc:backward(bencode[{{},i,j}],allc.gradInput[{{},i,j}])
+         print "================= after gradinput =================="
+         print(gradInput[{{},i,j}])
+      end
    end
 end
